b5298e7953eff293e34e88d5ddbf31aa26428c18
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-08-21 01:25:55 -0500

    added more psuedo code comments to map out how I want to do this project before starting

--- Main/index.js
+++ Main/index.js
@@ -2,3 +2,25 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
 
+//Manager
+//What is the Team Manager's name?
+//id? email? office number?
+
+//Which type of team member would you like to add(list)
+//Engineer, Intern, don't want to add any more members
+
+//Engineer
+//Name? id? email? Github?
+
+//Which type of team member would you like to add(list)
+//Engineer, Intern, don't want to add any more members
+
+//Intern
+//name? id? email? school?
+
+//Which type of team member would you like to add(list)
+//Engineer, Intern, don't want to add any more members
+//Maybe a success message after finishing the team profile
+
+//Then creates html file in output folder
+//use regualr html codie just like the read-me Generator
